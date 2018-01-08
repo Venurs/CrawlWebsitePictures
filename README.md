d145e8a4341bc202dff076e58d545157b8bfb780
Venurs <1411716329@qq.com>
2018-01-08 21:41:45 +0800

    多线程爬取进行中

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # CrawlWebsitePictures
-?????????
-1.????????????????????????????
-2.??????????????????
+
+2. 使用多线程以及python爬虫实现爬取微信搜索中义“python爬虫"为关键字的文章
+
+搜索链接为：http://weixin.sogou.com/weixin?query=python+爬虫 &type=2&page=7
+
+其中page 代表搜索到的有关文章的页数
+
+内容链接实际格式为： http://mp.weixin.qq.com/s?src=11&amp;timestamp=1515411914&amp;ver=624&amp;signature=BuB-RqLFORvJJDrVgU7Hj1UuCds2nN3cbb*4wg4nYa-hAMYpIhXsq1IyyTmVZtlfukxtOBP4bE-WObQ5hnds95pHQFssPeM8DKUxbhGq*NoPk5dU2wUmsYJWVS7BvqVu&amp;new=1(需处理去掉其中的“&amp”)
+
+内容链接匹配字符串：<a target="_blank" href="http://mp.weixin.qq.com/s?src=11&amp;timestamp=1515411914&amp;ver=624&amp;
+signature=BuB-RqLFORvJJDrVgU7Hj1UuCds2nN3cbb*4wg4nYa-hAMYpIhXsq1IyyTmVZtlfukxtOBP4bE-WObQ5hnds95pHQFssPeM8DKUxbhGq*NoPk5dU2wUmsYJWVS7BvqVu&amp;new=1" id="sogou_vr_11002601_title_0"
+
+
+
---
+++ threading/get_weichat_title.py
@@ -0,0 +1,100 @@
+from urllib import request, parse, error
+import re
+import time
+
+#  模拟浏览器
+header = ("User-Agent", "Mozilla/5.0 (Windows NT 6.1; W…) Gecko/20100101 Firefox/57.0")
+opener = request.build_opener()
+
+opener.addheaders = [header]
+request.install_opener(opener)  # 将opener安装为全局
+
+article_list = []  # 文章列表，用于存储每一页的文章链接
+
+
+#  使用代理服务器
+def use_proxy(proxy_addr, url):
+    """
+
+    :param proxy_addr: 代理服务器的地址
+    :param url: 待爬取的URL
+    :return: 返回链接爬到的数据
+    """
+    try:
+        proxy = request.ProxyHandler({"http": proxy_addr})
+        opener = request.build_opener(proxy, request.ProxyHandler)
+        request.install_opener(opener)
+        data = request.urlopen(url=url).read().decode("utf-8")
+        return data
+    except error.URLError as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        if hasattr(e, "reason"):
+            print(e.reason)
+        print(use_proxy.__doc__)
+        time.sleep(5)
+    except Exception as e:
+        print("exception" + e)
+        print(use_proxy.__doc__)
+        time.sleep(2)
+
+
+
+def get_articles(key, pagestart, pageend, proxy_addr):
+    """
+
+    :param key: url中的关键字:python+爬虫(只需要将爬虫进行二进制编码)
+    :param pagestart: 搜索到的文章的开始页为1
+    :param pageend: 搜索到的文章的结束页为10
+    :param proxy_addr: 代理服务器地址
+    :return: 返回搜索的文章的链接地址
+    """
+    try:
+        key = request.quote(key)
+        for page in range(pagestart, pageend + 1):
+            url = "http://weixin.sogou.com/weixin?query=" + str(key) + "&type=2&page=" + str(page)
+            data = use_proxy(proxy_addr, url)
+            reg = '<target="_blank" href="?(http://mp.weixin.qq.com/s?src=11.*)"'
+            article_list.append(re.compile(reg, ).findall(data))
+        print("article的长度为：", len(article_list))
+        return article_list
+    except error.URLError as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        if hasattr(e, "reason"):
+            print(e.reason)
+        print(get_articles.__doc__)
+        time.sleep(5)
+    except Exception as e:
+        print("exception" + e)
+        print(get_articles.__doc__)
+        time.sleep(2)
+
+
+def get_content(proxy_addr, article_list):
+    """
+    获取文章链接下的文章内容并保存为网页文件
+    :param proxy_addr:代理服务器地址
+    :param article_list: 存储文章链接的列表(连接中含有待处理项)
+    :return:
+    """
+    try:
+        #  网页头部
+        html = "<!doctype html><html><head><title>微信文章网页</title></head><body>"
+        #  i为页码，j为对应页的第几篇文章
+        for i in range(0, len(article_list)):
+            for j in range(0, len(i)):
+                # 处理拿到的链接，将其中的"&amp;"去除掉
+                url = article_list[i][j]
+                url = url.replace("amp;", "")
+
+
+
+
+
+
+
+
+
+
+
